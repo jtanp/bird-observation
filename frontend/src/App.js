9c987f39a6398371edbf6e018ea585bc13690e13
Joonatan Peltonen <joonatan.peltonen@gmail.com>
2021-04-03 23:14:59 +0300

    Added about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import ObservationContainer from './containers/ObservationContainer';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
-        <ObservationContainer/>
+        <Route path="/" exact component={ObservationContainer} />
+        <Route path="/about" exact component={About} />
       </Router>
     </div>
   );
---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const About = () => {
+    return (
+        <div className="about">
+            <p>
+                This website saves bird observations.
+            </p>
+        </div>
+    )
+}
+
+export default About;
