40158e1b6aa3bf45617652abfe1be2fc61ce2f8e
Joonatan Peltonen <joonatan.peltonen@gmail.com>
2021-05-01 23:09:31 +0300

    Modified about page

---
+++ frontend/src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <p>This website saves bird observations.</p>
+    </div>
+  );
+};
+
+export default About;
